14165625d273b8182836a9f5115a6b33121b1d46
Gopal Shah <simplecooldude1@gmail.com>
2016-08-03 11:59:46 -0700

    add new view

---
+++ Snip/URLDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  URLDetailViewController.swift
+//  Snip
+//
+//  Created by Kabir Shah on 8/3/16.
+//  Copyright © 2016 Kabir Shah. All rights reserved.
+//
+
+import UIKit
+
+class URLDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
