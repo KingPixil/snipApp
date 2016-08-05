5127d77e3ec98a83efbe7d7af0d760eec4ce17d9
Gopal Shah <simplecooldude1@gmail.com>
2016-08-05 12:08:27 -0700

    share icon and success message bigger

--- Snip/URLDetailViewController.swift
+++ Snip/URLDetailViewController.swift
@@ -25,6 +25,7 @@ class URLDetailViewController: UIViewController {
         snippedUrl = convertStringToDictionary(String(url!))!
         
         linkButton.setTitle(snippedUrl?["snippedURL"] as! String, forState: .Normal)
+        shareButton.setImage(UIImage(named: "ShareIcon") as UIImage?, forState: .Normal)
     }
 
     override func didReceiveMemoryWarning() {
