229aae811e94c57c00ee44258a53a6d87051c419
Gopal Shah <simplecooldude1@gmail.com>
2016-08-19 18:32:15 -0700

    http on new URL

--- Snip/ViewController.swift
+++ Snip/ViewController.swift
@@ -70,11 +70,11 @@ class ViewController: UIViewController, UITextFieldDelegate, UINavigationControl
         
         let url : NSURL?
         if httpCheck == "http://" {
-            url = NSURL(string: "https://snipit.ga/shorten/v1?url=" + urlToShorten!)
+            url = NSURL(string: "http://snipit.ga/shorten/v1?url=" + urlToShorten!)
         } else if httpsCheck == "https://" {
-            url = NSURL(string: "https://snipit.ga/shorten/v1?url=" + urlToShorten!)
+            url = NSURL(string: "http://snipit.ga/shorten/v1?url=" + urlToShorten!)
         } else {
-            url = NSURL(string: "https://snipit.ga/shorten/v1?url=http://" + urlToShorten!)
+            url = NSURL(string: "http://snipit.ga/shorten/v1?url=http://" + urlToShorten!)
         }
         
         let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
